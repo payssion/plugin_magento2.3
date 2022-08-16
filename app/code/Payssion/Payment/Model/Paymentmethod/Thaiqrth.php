6bbe2668d0ba97cc6079b304d08681d24cb64322
payssion <liudong4u@gmail.com>
2022-08-16 18:41:11 +0800

    add promptpay_th, thaiqr_th, shopeepay_th

---
+++ app/code/Payssion/Payment/Model/Paymentmethod/Thaiqrth.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © 2016 Payssion All rights reserved.
+ */
+
+namespace Payssion\Payment\Model\Paymentmethod;
+
+class Thaiqrth extends PaymentMethod
+{
+	protected $_code = 'payssion_payment_thaiqr_th';
+}
