27fccad935ffe9de5ce431cd94848d360df1f655
payssion <liudong4u@gmail.com>
2024-02-19 10:08:35 +0800

    add naverpay_kr, payid_au

---
+++ app/code/Payssion/Payment/Model/Paymentmethod/Naverpaykr.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © 2016 Payssion All rights reserved.
+ */
+
+namespace Payssion\Payment\Model\Paymentmethod;
+
+class Naverpaykr extends PaymentMethod
+{
+	protected $_code = 'payssion_payment_naverpay_kr';
+}
---
+++ app/code/Payssion/Payment/Model/Paymentmethod/Payidau.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © 2016 Payssion All rights reserved.
+ */
+
+namespace Payssion\Payment\Model\Paymentmethod;
+
+class Payidau extends PaymentMethod
+{
+	protected $_code = 'payssion_payment_payid_au';
+}
